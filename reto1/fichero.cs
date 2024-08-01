bb96c6a9bef130d1d28a5958817515860e0e32d7
Rubén Rafael Galán Sanz <ru811986@gmail.com>
2024-08-01 21:25:47 +0200

    Create fichero.cs

---
+++ reto1/fichero.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace ejemplo001
+{
+    class Fichero001
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bienvenidos a C#");
+        }
+    }
+}
