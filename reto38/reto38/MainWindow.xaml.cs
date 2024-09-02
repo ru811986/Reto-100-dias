29ec5c4228a1dba38bb0b4b510eea932c8a7290b
Rubén Rafael Galán Sanz <ru811986@gmail.com>
2024-09-02 16:34:11 +0200

    Add files via upload

---
+++ reto38/reto38/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace reto38
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSaludar_Click(object sender, RoutedEventArgs e)
+        {
+            if (txtNombre.Text.Trim().Length > 0)
+            {
+                lblSaludo.Content = $"Bienvenid@ {txtNombre.Text.Trim()}";
+                txtNombre.Text = "";
+            }
+            else
+            {
+                lblSaludo.Content = "Bienvenid@";
+            }
+        }
+    }
+}
